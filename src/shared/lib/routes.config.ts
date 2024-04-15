4b00a4d930ba4a796e20464ac93312b4734d68ee
Nursultan <nurlanov0105@gmail.com>
2024-04-15 21:34:08 +0600

    fixed conflict

--- src/shared/lib/routes.config.ts
+++ src/shared/lib/routes.config.ts
@@ -1,8 +1,7 @@
 export enum MARKETPLACE {
    EQUIPMENT = "/marketplace/equipment",
    SERVICE = `/marketplace/service`,
-   ORDER = `/marketplace/order`,
-   CREATE_ORDER = `/marketplace/create-order`
+   CREATE_ORDER = `/marketplace/create-order`,
 }
 
 export enum DASHBOARD {
