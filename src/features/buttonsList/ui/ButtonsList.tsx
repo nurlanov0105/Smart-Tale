87de7674ca119e6dc557ac2c8c4ae0ef22ac9309
Nursultan <nurlanov0105@gmail.com>
2024-04-16 17:30:47 +0600

    fixed conflict

--- src/features/buttonsList/ui/ButtonsList.tsx
+++ src/features/buttonsList/ui/ButtonsList.tsx
@@ -4,33 +4,26 @@ import clsx from "clsx";
 import styles from "./styles.module.scss";
 import { ButtonsProps } from "../model/types";
 
+const ButtonsList = ({ type, setType, values }: ButtonsProps) => {
+   const handleType = (newType: string) => setType(newType);
+   const isActive = (value: string) => {
+      return clsx(styles.order__button, {
+         ["active"]: value === type,
+      });
+   };
 
-const ButtonsList = ({type, setType, values}: ButtonsProps) => {
-
-    const handleType = (newType: string) => setType(newType)
-    const isActive = (value: string) => {
-        return clsx(styles.order__button, {
-            ['active']: value === type,
-        })
-    }
-
-    return (
-        <div className={styles.buttons}>
-            {
-                values.map(value =>
-                    <BtnBordered
-                        key={value.postValue}
-                        onClick={() => handleType(value.postValue)}
-                        className={isActive(value.postValue)}>{value.value}
-                    </BtnBordered>
-                )
-            }
-
-        </div>
-    );
-
->>>>>>> d66f926170f19fe965a2421b35a7281168f30e4f:src/entities/buttonsList/ui/ButtonsList.tsx
-
+   return (
+      <div className={styles.buttons}>
+         {values.map((value) => (
+            <BtnBordered
+               key={value.postValue}
+               onClick={() => handleType(value.postValue)}
+               className={isActive(value.postValue)}>
+               {value.value}
+            </BtnBordered>
+         ))}
+      </div>
+   );
 };
 
 export default ButtonsList;
