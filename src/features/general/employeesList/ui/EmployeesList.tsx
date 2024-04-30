64e87e88de15277adb539b7c31283ed3ecd85735
PranovAdilet <pranov.adiletqwe@mail.ru>
2024-04-30 19:33:40 +0600

    added skeletons

--- src/features/general/employeesList/ui/EmployeesList.tsx
+++ src/features/general/employeesList/ui/EmployeesList.tsx
@@ -1,28 +1,26 @@
 import React from "react";
 import { EmployeesItem } from "@/entities/admin/employeesItem";
-import styles from "./styles.module.scss";
 import {employeesCategories} from "@/features/general/employeesList/model/values.data";
-import {CommonSkeleton} from "@/shared/ui";
+import styles from "./styles.module.scss";
 
 const EmployeesList = () => {
    const list = [1, 2, 3, 4, 5];
 
    return (
-      // <div className={styles.list}>
-      //    <ul className={styles.list__top}>
-      //       {
-      //          employeesCategories.map(category =>
-      //              <li key={category} className={styles.list__item}>{category}</li>
-      //          )
-      //       }
-      //    </ul>
-      //
-      //    {list.map((item) => (
-      //       <EmployeesItem key={item} item={item} />
-      //    ))}
-      //
-      // </div>
-       <CommonSkeleton type="table"/>
+      <div className={styles.list}>
+         <ul className={styles.list__top}>
+            {
+               employeesCategories.map(category =>
+                   <li key={category} className={styles.list__item}>{category}</li>
+               )
+            }
+         </ul>
+
+         {list.map((item) => (
+            <EmployeesItem key={item} item={item} />
+         ))}
+
+      </div>
 
    );
 };
