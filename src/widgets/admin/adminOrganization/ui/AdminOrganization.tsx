33179b5687cd282d1908cd159234c62b35cbf987
Nursultan <nurlanov0105@gmail.com>
2024-06-24 22:06:06 +0600

    updates

--- src/widgets/admin/adminOrganization/ui/AdminOrganization.tsx
+++ src/widgets/admin/adminOrganization/ui/AdminOrganization.tsx
@@ -9,8 +9,8 @@ import { ORGANIZATION_ROUTES, useConfettiStore } from "@/shared/lib";
 import { useRouter } from "next/navigation";
 import { ConfettiComponent } from "@/entities/general/confetti";
 import { useOrganization } from "@/widgets/admin/adminOrganization/model/useOrganization";
-import {useSubscribeStore} from "@/shared/store/subscribeStore/subscribeStore";
-import {ErrorMessage} from "@/entities/general/errorMessage";
+import { useSubscribeStore } from "@/shared/store/subscribeStore/subscribeStore";
+import { ErrorMessage } from "@/entities/general/errorMessage";
 import styles from "./styles.module.scss";
 
 const AdminOrganization: FC = () => {
@@ -19,23 +19,24 @@ const AdminOrganization: FC = () => {
    const organizations = data && data.my_orgs;
    const activeOrg = data && data["orgs_active"].find((item) => item.active);
 
-   const positions = useSubscribeStore(state => state.data?.job_titles)
-   const isSubscribe = useSubscribeStore(state => state.isSubscribe)
-   const subscription = useSubscribeStore(state => state.data?.subscription)
+   const positions = useSubscribeStore((state) => state.data?.job_titles);
+   const isSubscribe = useSubscribeStore((state) => state.isSubscribe);
+   const subscription = useSubscribeStore((state) => state.data?.subscription);
    const isShow = useConfettiStore((state) => state.isShow);
    const endConfetti = useConfettiStore((state) => state.endConfetti);
 
    const MAX_ORGANIZATIONS_CREATING = 5;
    const MIN_ORGANIZATIONS_CREATING = 1;
    const isShowButton = () => {
-      if (isSubscribe && subscription && positions?.length === MIN_ORGANIZATIONS_CREATING){
-         return
+      if (isSubscribe && subscription && positions?.length === MIN_ORGANIZATIONS_CREATING) {
+         return;
       }
-      if (isSubscribe && !subscription && positions?.length === MAX_ORGANIZATIONS_CREATING){
-         return
+
+      if (isSubscribe && !subscription && positions?.length === MAX_ORGANIZATIONS_CREATING) {
+         return;
       }
-      return true
-   }
+      return true;
+   };
 
    const { push } = useRouter();
    const handleAdd = () => push(ORGANIZATION_ROUTES.CREATE_ORGANIZATION);
@@ -50,7 +51,7 @@ const AdminOrganization: FC = () => {
    }, []);
 
    if (isLoading) return <GlobalLoading type="default" />;
-   if (isError) return <ErrorMessage/>;
+   if (isError) return <ErrorMessage />;
 
    return (
       <>
@@ -60,9 +61,7 @@ const AdminOrganization: FC = () => {
             <div className={styles.list}>
                <div className={styles.list__top}>
                   <h4 className="h4">Список организаций</h4>
-                  {
-                      isShowButton() && <Button onClick={handleAdd}>Добавить организацию</Button>
-                  }
+                  {isShowButton() && <Button onClick={handleAdd}>Добавить организацию</Button>}
                </div>
                {organizations
                   ?.map((item) => (
